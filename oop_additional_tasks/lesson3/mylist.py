727c5fb92401fd116fb911c69b8c8b8688c52299
konstantin <kostyani4y@yandex.ru>
2026-06-11 22:45:03 +0300

    lesson3 прорешал

--- oop_additional_tasks/lesson3/mylist.py
+++ oop_additional_tasks/lesson3/mylist.py
@@ -3,7 +3,7 @@
 
 - __init__(self, data): конструктор, принимающий список элементов;
 - __repr__(self): магический метод, возвращающий строковое представление списка,
-которое можно использовать для создания нового объекта класса MyList;
+  его можно использовать для создания нового объекта класса MyList;
 - __str__(self): магический метод, возвращающий строковое представление списка;
 - __len__(self): магический метод, возвращающий длину списка;
 - __add__(self, other): магический метод, который позволяет складывать списки и возвращать новый список.
@@ -11,15 +11,51 @@
 
 
 class MyList:
-    pass
+    """Класс-обёртка над стандартным списком для изучения магических методов"""
+
+    def __init__(self, data: list):
+        """Конструктор, принимающий список элементов"""
+        # Превращаем входные данные в настоящий список, чтобы избежать мутаций
+        self.data = list(data)
+
+
+    def __repr__(self) -> str:
+        """Магический метод для технического представления объекта"""
+        return f"MyList({self.data})"
+
+
+    def __str__(self) -> str:
+        """Магический метод для пользовательского отображения списка"""
+        return str(self.data)
+
+
+    def __len__(self) -> int:
+        """Магический метод, возвращающий длину списка"""
+        return len(self.data)
+
+
+    def __add__(self, other: "MyList" | list) -> "MyList":
+        """Магический метод для сложения списков.
+        Возвращает новый объект класса MyList.
+        """
+        if isinstance(other, MyList):
+            # Если складываем два объекта MyList
+            new_data = self.data + other.data
+        elif isinstance(other, list):
+            # Если к MyList прибавляем обычный список [1, 2, 3]
+            new_data = self.data + other
+        else:
+            raise TypeError("Складывать можно только с MyList или со списком (list).")
+
+        return MyList(new_data)
 
 
 # код для проверки 
 my_list1 = MyList([1, 2, 3])
-print(repr(my_list1))  # MyList([1, 2, 3])
-print(str(my_list1))  # [1, 2, 3]
-print(len(my_list1))  # 3
+print(repr(my_list1))           # >>> MyList([1, 2, 3])
+print(str(my_list1))            # >>> [1, 2, 3]
+print(len(my_list1))            # >>> 3
 
 my_list2 = MyList([4, 5, 6])
 my_list3 = my_list1 + my_list2
-print(my_list3)  # [1, 2, 3, 4, 5, 6]
+print(my_list3)                 # >>> [1, 2, 3, 4, 5, 6]
